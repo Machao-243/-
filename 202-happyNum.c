2d3fa9a06a25a705619f45e435f473486b5b309b
Machao-243 <2432940552@qq.com>
2026-09-12 10:54:11 +0800

    202快乐数代码修改

--- 202-happyNum.c
+++ 202-happyNum.c
@@ -1,3 +1,4 @@
+//快慢指针法，只要两个指针相遇那一定有环，在第一个重复元素相遇，判断那个元素是否是一。
 int getNext(int n){
     int sum = 0;
     while(n > 0){
@@ -17,4 +18,24 @@ bool isHappy(int n) {
         fastp = getNext(fastp);
     }while(fastp != slowp);
     return slowp == 1;
+}
+//哈希表筛重。
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> seen = new HashSet<>();
+        while(n != 1 && !seen.contains(n)){
+            seen.add(n);
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+    private int getNext(int n){
+        int sum = 0;
+        while(n > 0){
+            int d = n % 10;
+            sum += d * d;
+            n = n / 10;
+        }
+        return sum;
+    }
 }
