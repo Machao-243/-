9d752f33c3c1787a770c53ebe9db783696b301dd
Machao-243 <2432940552@qq.com>
2026-09-15 13:54:04 +0800

    二分法求完全平方数。

---
+++ 367-isPerfectSquare.c
@@ -0,0 +1,16 @@
+bool isPerfectSquare(int num) {
+    int left = 0;
+    int right = num;
+    while(left <= right){
+        int mid = left + (right - left) / 2;
+        long square = (long) mid * mid;
+        if(square < num){
+            left = mid + 1;
+        }else if(square > num){
+            right = mid - 1;
+        }else{
+            return true;
+        }
+    }
+    return false;
+}
