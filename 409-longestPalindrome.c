d03e503409cb9c26453e3cf2559066d7f2991ece
Machao-243 <2432940552@qq.com>
2026-09-19 11:24:28 +0800

    求最长回文串的长度，出现偶次数的字母它一定都可以纳入长度当中，出现奇数次的字母只需要一个来作为中心。用哈希表存字符串的字母。

---
+++ 409-longestPalindrome.c
@@ -0,0 +1,18 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        int length = s.length();
+        for(int i = 0; i < length; i++){
+            char c = s.charAt(i);
+            count[c]++;
+        }
+        int ans = 0;
+        for(int v : count){
+            ans += v / 2 * 2;
+            if(ans % 2 == 0 && v % 2 == 1){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
